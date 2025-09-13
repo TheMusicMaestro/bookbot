056a0c00b7cd2645be43eda4197c15c37a972f50
Adam Huckeby <Adam@AdamHuckeby.com>
2025-09-12 22:05:58 -0500

    update main.py with count_words function

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+def count_words(book_text):
+    split_words = book_text.split()
+    total_words = 0
+    for word in split_words:
+        total_words += 1
+    return total_words
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    # define variables
+    book_source = "books/frankenstein.txt"
+    book_text = get_book_text(book_source)
+    word_count = count_words(book_text)
+
+    # print content to console
+    print(f"{book_text}")
+    print(f"{word_count} words found in the document")
 
 main()
